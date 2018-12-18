8c77fc64af097aad46eae9e64668170c0dcc2094
maty21 <maty21@gmail.com>
2018-12-17 19:14:31 -1000

    aaa

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Tests from './Tests';
+import Layout from './Layout';
+//import 'antd/dist/antd.css'; 
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+const client = new ApolloClient({
+  uri: "http://localhost:4000/"
+});
+ReactDOM.render(
+ <ApolloProvider client={client}>
+{/* <App /> */}
+<Layout/>
+</ApolloProvider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
